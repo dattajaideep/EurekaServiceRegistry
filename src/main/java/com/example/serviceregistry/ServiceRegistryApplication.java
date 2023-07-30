8eb257817fd500bb21af0a703ffec1ebceea2a0f
dattajaideep <varanasidattasaijaideep@gmail.com>
2023-07-31 00:25:31 +0530

    Updated Package name and Properties

--- src/main/java/com/example/serviceregistry/ServiceRegistryApplication.java
+++ src/main/java/com/example/serviceregistry/ServiceRegistryApplication.java
@@ -1,4 +1,4 @@
-package com.example.ServiceRegistry;
+package com.example.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/serviceregistry/ServiceRegistryApplicationTests.java
+++ src/test/java/com/example/serviceregistry/ServiceRegistryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.ServiceRegistry;
+package com.example.serviceregistry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
